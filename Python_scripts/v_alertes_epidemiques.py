32b67ae80c38f87b07036c17f619247e4767238c
atchom <36500277+atchom@users.noreply.github.com>
2026-04-28 13:50:16 +0200

    Add files via upload

---
+++ Python_scripts/v_alertes_epidemiques.py
@@ -0,0 +1,27 @@
+import psycopg2
+import pandas as pd
+import psycopg2
+import pandas as pd
+
+# Configuration de connexion PostgreSQL
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="DHIS2_LT_CMR",
+    user="postgres",
+    password="Ida1982@#"
+)
+# Supprimez cette ligne qui cause l'erreur
+# conn = psycopg2.connect(conn)
+
+cursor = conn.cursor()
+
+# cursor.execute(open("create_view.sql", "r", encoding="utf-8").read())
+df = pd.read_sql("SELECT * FROM v_alertes_epidemiques", conn)
+
+print(df.head())
+conn.commit()
+cursor.close()
+conn.close()
+
+df.to_csv(r"C:\\CIC_Github\\DHIS2_DB\\CSV\\SRC_v_alertes_epidemiques.csv", index=False, encoding='utf-8-sig')
